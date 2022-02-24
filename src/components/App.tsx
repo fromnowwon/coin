9495ffa5cddb2dd3c27c7b676112656ee9588022
cw <fromnowwon@gmail.com>
2022-02-24 20:36:06 +0900

    update

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './App.scss';
+import '../App.scss';
 
 interface CoinsType {
 	rank: number,
@@ -56,8 +56,8 @@ function App(): JSX.Element {
 							<table>
 								<thead>
 									<tr>
-										<th>순위</th>
-										<th>종목</th>
+										<th className="headCol rankCol">순위</th>
+										<th className="headCol nameCol">종목</th>
 										<th>기호</th>
 										<th>가격(KRW)</th>
 										<th>총 시가</th>
@@ -70,8 +70,8 @@ function App(): JSX.Element {
 									{
 										coins.map((coin: CoinsType, idx: number) => (
 											<tr key={ idx }>
-												<td className="align-right">{ coin.rank }</td>
-												<td>{ coin.name }</td>
+												<td className="headCol rankCol align-right">{ coin.rank }</td>
+												<td className="headCol nameCol">{ coin.name }</td>
 												<td>{ coin.symbol }</td>
 												<td className="align-right">₩{ Number(coin.quotes.KRW.price.toFixed(1)).toLocaleString() }</td>
 												<td className="align-right">{ (coin.quotes.KRW.market_cap / 1000000000000).toFixed(2) }T</td>
